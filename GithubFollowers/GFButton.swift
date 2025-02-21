824261fb6e5c439cece7cd49fc40c8ff6aa35945
NovikovValeriy <novikov.valera2005@gmail.com>
2025-02-21 21:35:28 +0300

    search screen view controller

---
+++ GithubFollowers/GFButton.swift
@@ -0,0 +1,20 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Валерий Новиков on 21.02.25.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GithubFollowers/GFTextField.swift
@@ -0,0 +1,20 @@
+//
+//  GFTextField.swift
+//  GithubFollowers
+//
+//  Created by Валерий Новиков on 21.02.25.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
